ed7f09c94fbf197fbcc32ef04fe80a9b470706ba
David <litlmoz@gmail.com>
2016-01-29 14:18:32 -0800

    added static pages

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get bio" do
+    get :bio
+    assert_response :success
+  end
+
+end
