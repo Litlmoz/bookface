ad754ba76a0170cdd0ecaa0d65d4128a382b0b85
David <litlmoz@gmail.com>
2016-02-03 15:49:58 -0800

    changed password param to encrypted_password and added logout link

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20160203233002) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "email",                  default: "",    null: false
-    t.string   "password",               default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
