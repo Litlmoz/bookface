0c51fc081610b254b6bccccd18ca02ebb65723c0
David <litlmoz@gmail.com>
2016-02-03 15:29:01 -0800

    scaffold user model w/admin boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202164310) do
+ActiveRecord::Schema.define(version: 20160203232604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,12 @@ ActiveRecord::Schema.define(version: 20160202164310) do
   end
 
   add_index "photos", ["presentation_order"], name: "index_photos_on_presentation_order", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "admin",      default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
 end
