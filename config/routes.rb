fc85a09300043125048191838e9a43eac6efc72b
David <litlmoz@gmail.com>
2016-02-04 00:14:43 -0800

    added padding around blog column with columns

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
 
   get 'page/about'
 
+  resources :photos, :blogs, :users
+
   devise_for :users
 
-  resources :photos, :blogs, :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :login
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
