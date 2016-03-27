ea5cccfbc13476ca2f7f82804c5826feda8b4414
David Solis <david@soliskit.com>
2016-03-27 14:13:35 -0700

    fixed logout route spec

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe RegistrationsController, type: :routing do
     end
 
     it "routes to #logout" do
-      expect(:get => "/logout").to route_to("devise/sessions#destroy")
+      expect(:delete => "users/sign_out").to route_to("devise/sessions#destroy")
     end
 
     it "routes to #new" do
